6881e9d24b1b80ec88435b069bcc2d7c9045ffe9
Łukasz Jędrasik <lukasz.jedrasik@icloud.com>
2023-04-24 10:04:42 +0200

    Update apps/web/styles/fonts.ts
    Co-authored-by: Andrzej Kurek <32792205+Razz21@users.noreply.github.com>

--- apps/web/styles/fonts.ts
+++ apps/web/styles/fonts.ts
@@ -1,13 +1,11 @@
 import { Red_Hat_Display, Red_Hat_Text } from 'next/font/google';
 
 export const fontBody = Red_Hat_Text({
-  subsets: ['latin-ext'],
-  weight: ['400', '500', '700'],
+  subsets: ['latin'],
   variable: '--font-body',
 });
 
 export const fontHeadings = Red_Hat_Display({
-  subsets: ['latin-ext'],
-  weight: ['400', '500', '700'],
+  subsets: ['latin'],
   variable: '--font-headings',
 });
