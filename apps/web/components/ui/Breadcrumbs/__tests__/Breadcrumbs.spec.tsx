4d1d19e18143f162e8a4abb3b3bf636549fe5921
Dawid Ziobro <dziobro@vuestorefront.io>
2023-04-26 12:26:21 +0200

    feat: add breadcrumbs block

---
+++ apps/web/components/ui/Breadcrumbs/__tests__/Breadcrumbs.spec.tsx
@@ -0,0 +1,29 @@
+import { fireEvent, render } from '@testing-library/react';
+import { Breadcrumbs } from '../Breadcrumbs';
+
+const breadcrumbs = [
+  {
+    name: 'Home',
+    link: '/',
+  },
+];
+
+describe('<Breadcrumbs />', () => {
+  it('should render component', () => {
+    const { getByTestId } = render(<Breadcrumbs breadcrumbs={breadcrumbs} />);
+
+    expect(getByTestId('breadcrumbs')).toBeInTheDocument();
+  });
+
+  it('should toggle dropdown', () => {
+    const { getByTestId, queryByTestId } = render(<Breadcrumbs breadcrumbs={breadcrumbs} />);
+
+    expect(queryByTestId('breadcrumbsDropdown')).not.toBeInTheDocument();
+
+    const dropdownButton = getByTestId('breadcrumbsDropdownButton');
+
+    fireEvent.click(dropdownButton);
+
+    expect(queryByTestId('breadcrumbsDropdown')).toBeInTheDocument();
+  });
+});
