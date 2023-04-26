402e5cbaa66ed6722177418bfda89a7109c1dc51
ljedrasik <ljedrasik@vuestorefront.io>
2023-04-26 16:36:47 +0200

    chore: fix NavbarTop for layouts

--- apps/web/components/ui/NavbarTop/NavbarTop.tsx
+++ apps/web/components/ui/NavbarTop/NavbarTop.tsx
@@ -1,51 +1,23 @@
 import Link from 'next/link';
-import { Search, VsfLogo } from '@/components';
-import { SfIconShoppingCart, SfIconExpandMore, SfButton } from '@storefront-ui/react';
-import { useTranslation } from 'next-i18next';
+import { VsfLogo } from '@/components';
+import type { NavbarTopProps } from '@/components/ui/NavbarTop/types';
+import classNames from 'classnames';
 
-export function NavbarTop() {
-  const { t } = useTranslation();
-  const actionItems = [
-    {
-      icon: <SfIconShoppingCart />,
-      label: '',
-      ariaLabel: 'Cart',
-      role: 'button',
-    },
-  ];
-
+export function NavbarTop({ children, filled }: NavbarTopProps) {
   return (
-    <div className="w-full h-full bg-neutral-50 md:sticky md:-top-2.5 md:pt-0">
-      <header className="flex justify-center w-full text-white border-0 bg-primary-700 h-14 md:h-20 border-neutral-200">
-        <div className="flex gap-[clamp(1rem,3vw,3rem)] items-center w-full max-w-screen-3xl py-6 px-4 md:px-6 lg:px-10 mx-auto sticky top-0">
-          <Link href="/" aria-label="SF Homepage" className="h-6 md:h-7 -mt-1.5">
-            <VsfLogo />
-          </Link>
-          <SfButton
-            className="block !px-2 mr-auto text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white font-body hidden md:inline-flex"
-            type="button"
-            slotSuffix={<SfIconExpandMore />}
-            variant="tertiary"
-          >
-            <span>{t('allProductsLinkText')}</span>
-          </SfButton>
-          <Search className="hidden md:block flex-1" />
-          <nav className="hidden md:flex md:flex-row md:flex-nowrap">
-            {actionItems.map((actionItem) => (
-              <SfButton
-                className="mr-2 -ml-0.5 text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white"
-                key={actionItem.ariaLabel}
-                aria-label={actionItem.ariaLabel}
-                variant="tertiary"
-                slotPrefix={actionItem.icon}
-                square
-              >
-                {actionItem.role === 'login' && <p className="hidden md:inline-flex">{t(actionItem.label)}</p>}
-              </SfButton>
-            ))}
-          </nav>
-        </div>
-      </header>
-    </div>
+    <header
+      className={classNames(
+        'h-14 md:h-20 flex z-50 md:sticky md:-top-5 md:pt-2.5 md:shadow-md',
+        filled ? 'bg-primary-700 text-white' : 'bg-white text-[#02C652] border-b border-neutral-200',
+      )}
+      data-testid="navbar-top"
+    >
+      <div className="flex gap-[clamp(1rem,3vw,3rem)] items-center w-full md:h-[60px] max-w-screen-3xl py-6 px-4 md:px-6 lg:px-10 mx-auto sticky top-0">
+        <Link href="/" aria-label="SF Homepage" className="h-6 md:h-7 -mt-1.5">
+          <VsfLogo />
+        </Link>
+        {children}
+      </div>
+    </header>
   );
 }
--- apps/web/layouts/DefaultLayout.tsx
+++ apps/web/layouts/DefaultLayout.tsx
@@ -1,10 +1,45 @@
 import type { PropsWithChildren } from 'react';
-import { Footer, BottomNav, ScrollToTopButton, NavbarTop } from '@/components';
+import { Footer, BottomNav, ScrollToTopButton, NavbarTop, Search } from '@/components';
+import { SfButton, SfIconExpandMore, SfIconShoppingCart } from '@storefront-ui/react';
+import { useTranslation } from 'next-i18next';
 
 export function DefaultLayout({ children }: PropsWithChildren): JSX.Element {
+  const { t } = useTranslation();
+  const actionItems = [
+    {
+      icon: <SfIconShoppingCart />,
+      label: '',
+      ariaLabel: 'Cart',
+      role: 'button',
+    },
+  ];
   return (
     <>
-      <NavbarTop />
+      <NavbarTop filled>
+        <SfButton
+          className="block !px-2 mr-auto text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white font-body hidden md:inline-flex"
+          type="button"
+          slotSuffix={<SfIconExpandMore />}
+          variant="tertiary"
+        >
+          <span>{t('allProductsLinkText')}</span>
+        </SfButton>
+        <Search className="hidden md:block flex-1" />
+        <nav className="hidden md:flex md:flex-row md:flex-nowrap">
+          {actionItems.map((actionItem) => (
+            <SfButton
+              className="mr-2 -ml-0.5 text-white bg-transparent hover:bg-primary-800 hover:text-white active:bg-primary-900 active:text-white"
+              key={actionItem.ariaLabel}
+              aria-label={actionItem.ariaLabel}
+              variant="tertiary"
+              slotPrefix={actionItem.icon}
+              square
+            >
+              {actionItem.role === 'login' && <p className="hidden md:inline-flex">{t(actionItem.label)}</p>}
+            </SfButton>
+          ))}
+        </nav>
+      </NavbarTop>
       {/*<Breadcrumbs />*/}
       <main>{children}</main>
       <BottomNav />
