5fc1bb8100cb3b00eded81f5fa361109ef08b3e5
Andrzej Kurek <akurek@vuestorefront.io>
2023-05-04 14:34:20 +0200

    chore: sdk types

--- apps/web/sdk/index.ts
+++ apps/web/sdk/index.ts
@@ -1,28 +1,30 @@
 import { myModule, MyModuleType } from '@vsf-enterprise/integration-boilerplate-sdk';
-import { initSDK, buildModule, SDKConfig } from '@vsf-enterprise/sdk';
+import { initSDK, buildModule } from '@vsf-enterprise/sdk';
 import { SfProduct, SfProductReview } from '@vsf-enterprise/unified-data-model/src/product';
 
-const sdkConfig: SDKConfig = {
-  commerce: buildModule<MyModuleType>(
+const extension = {
+  extend: {
+    async getProduct({ slug }: { slug: string }): Promise<SfProduct> {
+      return import('~/mocks/product.json').then((response) => response.default);
+    },
+    async getProductReviews({ slug }: { slug: string }): Promise<SfProductReview[]> {
+      return import('~/mocks/reviews.json').then((response) => response.default);
+    },
+    async getProductRecommended({ slug }: { slug: string }): Promise<SfProduct[]> {
+      const product = await import('~/mocks/product.json').then((response) => response.default);
+      return Array.from({ length: 8 }, (_, index) => ({ ...product, id: index.toString() }));
+    },
+  },
+};
+
+const sdkConfig = {
+  commerce: buildModule<MyModuleType, typeof extension>(
     myModule,
     {
       apiUrl: 'http://localhost:4000/commerce',
       ssrApiUrl: 'http://localhost:4000/commerce',
     },
-    {
-      extend: {
-        async getProduct({ slug }: { slug: string }): Promise<SfProduct> {
-          return import('~/mocks/product.json').then((response) => response.default);
-        },
-        async getProductReviews({ slug }: { slug: string }): Promise<SfProductReview[]> {
-          return import('~/mocks/reviews.json').then((response) => response.default);
-        },
-        async getProductRecommended({ slug }: { slug: string }): Promise<SfProduct[]> {
-          const product = await import('~/mocks/product.json').then((response) => response.default);
-          return Array.from({ length: 8 }, (_, index) => ({ ...product, id: index.toString() }));
-        },
-      },
-    },
+    extension,
   ),
 };
 
