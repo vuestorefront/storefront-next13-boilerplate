33fbe6006f5e8b92f7fffd8ad426b9d3fc00bc65
Andrzej Kurek <32792205+Razz21@users.noreply.github.com>
2023-05-08 10:28:01 +0200

    chore: remove sdk extensions mock (#21)

--- apps/web/sdk/index.ts
+++ apps/web/sdk/index.ts
@@ -1,19 +1,8 @@
 import { myModule, MyModuleType } from '@vsf-enterprise/integration-boilerplate-sdk';
 import { initSDK, buildModule } from '@vsf-enterprise/sdk';
-import { SfProduct, SfProductReview } from '@vsf-enterprise/unified-data-model/src/product';
 
 const extension = {
   extend: {
-    async getProduct({ slug }: { slug: string }): Promise<SfProduct> {
-      return import('~/mocks/product.json').then((response) => response.default);
-    },
-    async getProductReviews({ slug }: { slug: string }): Promise<SfProductReview[]> {
-      return import('~/mocks/reviews.json').then((response) => response.default);
-    },
-    async getProductRecommended({ slug }: { slug: string }): Promise<SfProduct[]> {
-      const product = await import('~/mocks/product.json').then((response) => response.default);
-      return Array.from({ length: 8 }, (_, index) => ({ ...product, id: index.toString() }));
-    },
     async getContent({ url }: { url: string }): Promise<any[]> {
       return import('~/mocks/contentData.json').then((response) => response.default);
     },
