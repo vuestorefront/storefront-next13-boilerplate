ca33f497d7b598b7ba141fc9c850555c3023b40f
ljedrasik <ljedrasik@vuestorefront.io>
2023-05-09 14:39:25 +0200

    chore: remove comment

--- apps/web/sdk/index.ts
+++ apps/web/sdk/index.ts
@@ -2,14 +2,10 @@ import { myModule, MyModuleType } from '@vsf-enterprise/integration-boilerplate-
 import { initSDK, buildModule } from '@vsf-enterprise/sdk';
 
 const sdkConfig = {
-  commerce: buildModule<MyModuleType>(
-    myModule,
-    {
-      apiUrl: 'http://localhost:4000/commerce',
-      ssrApiUrl: 'http://localhost:4000/commerce',
-    },
-    // extension,
-  ),
+  commerce: buildModule<MyModuleType>(myModule, {
+    apiUrl: 'http://localhost:4000/commerce',
+    ssrApiUrl: 'http://localhost:4000/commerce',
+  }),
 };
 
 export const sdk = initSDK<typeof sdkConfig>(sdkConfig);
