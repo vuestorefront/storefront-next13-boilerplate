b1d8d1aa3d981b1a9d7b3003427296a472b702a1
Marcin Saja <marcin.saja.webmaster@gmail.com>
2023-05-10 09:09:20 +0200

    feat: fix unit test

--- apps/web/components/CartProductCard/__tests__/CartProductCard.spec.tsx
+++ apps/web/components/CartProductCard/__tests__/CartProductCard.spec.tsx
@@ -5,9 +5,13 @@ import { createWrapper } from '~/jest.utils';
 describe('<CartProductCard />', () => {
   it('should render component', () => {
     const createQueryClientWrapper = createWrapper();
-    const { getByTestId } = render(<CartProductCard data-testid="cart-product-card" />, {
-      wrapper: createQueryClientWrapper,
-    });
+    const { getByTestId } = render(
+      <CartProductCard
+        item={{ name: 'Sneaker', attributes: [], totalPrice: { amount: 123, currency: 'USD', precisionAmount: '123.00'}}}
+        data-testid="cart-product-card"
+      />,
+      { wrapper: createQueryClientWrapper, }
+    );
 
     expect(getByTestId('cart-product-card')).toBeInTheDocument();
   });
