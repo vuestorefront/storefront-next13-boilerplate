b68ef799bb2a4d49ff8ea011d8ea2b17ebc5444f
Łukasz Jędrasik <lukasz.jedrasik@icloud.com>
2023-05-10 10:29:42 +0200

    feat(react-1106): hp template cleanup (#25)
    * chore: update
    
    * chore: fix tests
    
    * chore: update types
    
    * chore: cleanup
    
    * chore: remove fake comments
    
    * chore: self review
    
    * chore: remove comment
    
    * chore: fixes
    
    * chore: fix test

--- apps/web/components/ui/Hero/__tests__/Hero.spec.tsx
+++ apps/web/components/ui/Hero/__tests__/Hero.spec.tsx
@@ -4,7 +4,7 @@ import { Hero } from '~/components';
 describe('<Hero />', () => {
   it('should render component', () => {
     const { getByTestId } = render(
-      <Hero data-testid="hero" title="" primaryButtonLink="" description="" image="" subtitle="" primaryButtonText="" secondaryButtonLink="" secondaryButtonText="" />,
+      <Hero data-testid="hero" title="" primaryButtonLink="" description="" image="/images/product.webp" subtitle="" primaryButtonText="" secondaryButtonLink="" secondaryButtonText="" />,
     );
 
     getByTestId('hero');
