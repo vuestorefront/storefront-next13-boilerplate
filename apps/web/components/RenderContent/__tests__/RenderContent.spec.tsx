75f541251604db9da0b27bf5d23e9aeb2ea4d44b
ljedrasik <ljedrasik@vuestorefront.io>
2023-05-10 13:07:04 +0200

    fix: remove unnecessary mock
    n

--- apps/web/components/RenderContent/__tests__/RenderContent.spec.tsx
+++ apps/web/components/RenderContent/__tests__/RenderContent.spec.tsx
@@ -1,22 +1,9 @@
 import { render } from '@testing-library/react';
 import { RenderContent } from '~/components';
-import type { ContentDynamicPage } from '~/hooks';
 
 describe('<RenderContent />', () => {
   it('should render component', () => {
-    const contentMock = [
-      {
-        fields: {
-          component: 'Page',
-          content: [{
-            fields: {
-              component: 'Test',
-            },
-          }]
-        }
-      }
-    ] as unknown as ContentDynamicPage['content'];
-    const { getByTestId } = render(<RenderContent content={contentMock} data-testid="render-content" />);
+    const { getByTestId } = render(<RenderContent content={[]} data-testid="render-content" />);
 
     getByTestId('render-content');
   });
