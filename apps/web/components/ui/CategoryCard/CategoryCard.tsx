af97c1c9805a5522806c066c077317f0e5d526a7
Łukasz Jędrasik <lukasz.jedrasik@icloud.com>
2023-05-12 10:03:24 +0200

    fix(react-1185): wrong card styling (#33)
    * fix: wrong card styling
    
    * chore: cr fixes

--- apps/web/components/ui/CategoryCard/CategoryCard.tsx
+++ apps/web/components/ui/CategoryCard/CategoryCard.tsx
@@ -9,19 +9,15 @@ export function CategoryCard({ items, ...attributes }: CategoryCardProps) {
       {...attributes}
     >
       {items.map(({ name, image, slug }) => (
-        <div className="relative min-w-[180px] flex-col max-w-[240px] mr-2 md:mr-6 group" key={name}>
+        <div className="mr-2 md:mr-6 group" key={name}>
           <NextLink
             className="w-full h-full z-1 focus-visible:outline focus-visible:outline-offset focus-visible:rounded-md"
             href={slug}
             aria-label={name}
           >
-            <Image
-              className="rounded-full bg-neutral-100 group-hover:shadow-xl group-active:shadow-none"
-              src={image}
-              alt={name}
-              width={240}
-              height={240}
-            />
+            <div className="relative h-[240px] w-[240px] rounded-full bg-neutral-100 group-hover:shadow-xl group-active:shadow-none">
+              <Image src={image} alt={name} layout="fill" />
+            </div>
             <div className="flex justify-center">
               <p className="mt-4 font-semibold no-underline text-normal-900 typography-text-base group-hover:underline group-hover:text-primary-800 group-hover:font-normal group-active:text-primary-800 group-active:font-normal">
                 {name}
