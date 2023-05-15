0510b2517601f8b8371650dd057c617ac2b633ef
Marcin Saja <marcin.saja.webmaster@gmail.com>
2023-05-15 14:08:39 +0200

    style: removed unneccesary wrapper

--- apps/web/components/CategoryPageContent/__tests__/CategoryPageContent.spec.tsx
+++ apps/web/components/CategoryPageContent/__tests__/CategoryPageContent.spec.tsx
@@ -1,12 +1,9 @@
 import { render } from '@testing-library/react';
 import { CategoryPageContent } from '~/components';
-import { createWrapper } from '~/jest.utils';
-
-const wrapper = createWrapper();
 
 describe('<CategoryPageContent />', () => {
   it('should render component', () => {
-    const { getByTestId } = render(<CategoryPageContent title="title" totalProducts={0} products={[]} />, { wrapper });
+    const { getByTestId } = render(<CategoryPageContent title="title" totalProducts={0} products={[]} />);
 
     expect(getByTestId('category-page-content')).toBeInTheDocument();
   });
