9ee1eee0efd3aee930b890e96adcab91f2bba6a4
Andrzej Kurek <32792205+Razz21@users.noreply.github.com>
2023-05-16 10:23:32 +0200

    fix: unit-tests (#47)

--- apps/web/components/ui/BottomNav/__tests__/BottomNav.spec.tsx
+++ apps/web/components/ui/BottomNav/__tests__/BottomNav.spec.tsx
@@ -1,6 +1,13 @@
 import { render } from '@testing-library/react';
 import { BottomNav } from '~/components';
 
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(() => ({
+    pathname: '/',
+    push: jest.fn(),
+  })),
+}));
+
 describe('<BottomNav />', () => {
   it('should render component', () => {
     const { getByTestId } = render(<BottomNav />);
