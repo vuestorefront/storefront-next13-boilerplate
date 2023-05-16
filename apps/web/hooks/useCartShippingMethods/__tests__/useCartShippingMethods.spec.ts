36e90553368f22c932116bf3b6ca08802eeab812
Dawid Ziobro <dziobro@vuestorefront.io>
2023-05-16 12:34:20 +0200

    test: fix useCartShippingMethods tests

--- apps/web/hooks/useCartShippingMethods/__tests__/useCartShippingMethods.spec.ts
+++ apps/web/hooks/useCartShippingMethods/__tests__/useCartShippingMethods.spec.ts
@@ -1,13 +1,41 @@
-import { renderHook } from '@testing-library/react';
-import { createWrapper } from '~/jest.utils';
+import { renderHook, waitFor } from '@testing-library/react';
 import { useCartShippingMethods } from '~/hooks';
+import { createWrapper } from '~/jest.utils';
 
 describe('useCartShippingMethods', () => {
   it('should return shipping methods', async () => {
     const wrapper = createWrapper();
     const { result } = renderHook(() => useCartShippingMethods(), { wrapper });
 
-    expect(result).toHaveProperty('current');
-    expect(result.current).toHaveProperty('shippingMethods');
+    await waitFor(() => expect(result.current.data).not.toBeUndefined());
+
+    expect(result.current.data).toMatchInlineSnapshot(`
+      {
+        "methods": [
+          {
+            "description": "",
+            "estimatedDelivery": "tomorrow",
+            "id": "1",
+            "name": "Standard",
+            "price": {
+              "amount": 3,
+              "currency": "USD",
+              "precisionAmount": "2",
+            },
+          },
+          {
+            "description": "",
+            "estimatedDelivery": "tomorrow",
+            "id": "2",
+            "name": "Express",
+            "price": {
+              "amount": 10,
+              "currency": "USD",
+              "precisionAmount": "2",
+            },
+          },
+        ],
+      }
+    `);
   });
 });
