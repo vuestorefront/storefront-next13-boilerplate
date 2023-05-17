4f17c88bd12e738a70245e0ae21f0c2dff96bdc2
daaf <44862757+dawid-ziobro@users.noreply.github.com>
2023-05-17 16:52:02 +0200

    fix: sequentially descending order (#55)

--- apps/web/components/CategorySorting/CategorySorting.tsx
+++ apps/web/components/CategorySorting/CategorySorting.tsx
@@ -7,12 +7,12 @@ export function CategorySorting() {
 
   return (
     <>
-      <h5
-        className="py-2 px-4 mb-6 bg-neutral-100 typography-headline-6 font-bold text-neutral-900 uppercase tracking-widest md:rounded-md"
+      <span
+        className="block py-2 px-4 mb-6 bg-neutral-100 typography-headline-6 font-bold text-neutral-900 uppercase tracking-widest md:rounded-md"
         data-testid="category-sorting"
       >
         {t('sortBy')}
-      </h5>
+      </span>
       <div className="px-2 mb-6">
         <SfSelect aria-label={t('sortBy')}>
           {sortingOptions.map((option) => (
