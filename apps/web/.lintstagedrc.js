e32a0794288fad0dad9b669dc015066acaed01c3
Marcin Saja <marcin.saja.webmaster@gmail.com>
2023-05-24 12:27:43 +0200

    test: separate ts config because of isolatedModules option

--- apps/web/.lintstagedrc.js
+++ apps/web/.lintstagedrc.js
@@ -1,8 +1,9 @@
 // https://nextjs.org/docs/basic-features/eslint#lint-staged
 module.exports = {
   "*.{ts,tsx}": [
-    // "bash -c tsc --skipLibCheck --noEmit",
-    // 'eslint --cache --cache-location .next/cache/eslint/ --fix'
+    "bash -c tsc --skipLibCheck --noEmit",
+    "bash -c tsc --skipLibCheck --noEmit -p __tests__",
+    'eslint --cache --cache-location .next/cache/eslint/ --fix'
   ],
-  // '*.{js,jsx}': ['eslint --cache --cache-location .next/cache/eslint/ --fix'],
+  '*.{js,jsx}': ['eslint --cache --cache-location .next/cache/eslint/ --fix'],
 };
