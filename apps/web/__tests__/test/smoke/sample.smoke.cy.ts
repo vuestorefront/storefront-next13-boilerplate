4740ad27849237890b2eefd850c706bdf88c7013
Maciej Pankiewicz <mpankiewicz@vuestorefront.io>
2023-05-26 12:16:12 +0200

    test(storefront-1236):homepage tests

--- apps/web/__tests__/test/smoke/sample.smoke.cy.ts
+++ apps/web/__tests__/test/smoke/sample.smoke.cy.ts
@@ -1,7 +1,23 @@
-describe('Smoke: Checkout', () => {
-    it('Sample test', () => {
-        cy.visit("/");
-    });
+import { HomePageObject } from "~/__tests__/support/pageObjects/HomePageObject";
 
-    //test
+const homePage = new HomePageObject();
+
+describe('Smoke: Boilerplate', () => {
+    describe('Homepage smoke test', () => {
+        it('[smoke] Check if Primary Button is working', () => {
+          homePage.checkPrimaryButton();
+        });
+        it('[smoke] Check if Secondary Button is working', () => {
+            homePage.checkSecondaryButton();
+        });
+        it('[smoke] Check if elements display properly', () => {
+            homePage.checkCategoryCard();
+            homePage.checkBanners();
+            homePage.checkProductCard();
+        });
+        it('[smoke] Check if Categories button is working ', () => {
+            homePage.checkHeaderCategory();
+      
+        });
+    });
 });
