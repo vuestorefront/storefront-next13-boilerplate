6638d26cda738c6666c53b27e8d2715c9ba30c64
porzechowski-vsf <127853075+porzechowski-vsf@users.noreply.github.com>
2023-05-26 07:50:57 +0200

    test(storefront-1234): configure cypress (#62)
    * test(storefront-1234): configure cypress
    
    * test: separate ts config because of isolatedModules option
    
    * test(storefront-1234): update lockfile
    
    ---------
    
    Co-authored-by: Marcin Saja <marcin.saja.webmaster@gmail.com>

--- apps/web/.lintstagedrc.js
+++ apps/web/.lintstagedrc.js
@@ -2,6 +2,7 @@
 module.exports = {
   "*.{ts,tsx}": [
     "bash -c tsc --skipLibCheck --noEmit",
+    "bash -c tsc --skipLibCheck --noEmit -p __tests__",
     'eslint --cache --cache-location .next/cache/eslint/ --fix'
   ],
   '*.{js,jsx}': ['eslint --cache --cache-location .next/cache/eslint/ --fix'],
