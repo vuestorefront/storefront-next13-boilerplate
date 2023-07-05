73255bbf58c33abb806dcbfbe06ca88edf369a98
lukasborawski <lukas.borawski@gmail.com>
2023-07-05 18:53:02 +0200

    chore: lint staged check rules

--- apps/web/.lintstagedrc.js
+++ apps/web/.lintstagedrc.js
@@ -1,18 +1,15 @@
-// lint-staged.config.js
-const { ESLint } = require("eslint");
+const { ESLint } = require('eslint');
 
 const removeIgnoredFiles = async (files) => {
   const eslint = new ESLint();
   const ignoredFiles = await Promise.all(files.map((file) => eslint.isPathIgnored(file)));
   const filteredFiles = files.filter((_, i) => !ignoredFiles[i]);
-  return filteredFiles.join(" ");
+  return filteredFiles.join(' ');
 };
 
 module.exports = {
-  "*.{ts,tsx}": [
-    "bash -c \"tsc --skipLibCheck --noEmit -p __tests__\"",
-  ],
-  "*.{js,ts}": async (files) => {
+  '*.{ts,tsx}': ['bash -c "tsc --skipLibCheck --noEmit -p __tests__"'],
+  '*.{js,ts,tsx}': async (files) => {
     const filesToLint = await removeIgnoredFiles(files);
     return [`eslint --max-warnings=0 ${filesToLint} --fix`];
   },
