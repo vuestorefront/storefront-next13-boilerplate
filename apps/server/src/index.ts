628f955acb278e450a03db1edb08b7bb713855a1
jason.wolf <jason.wolf@borngroup.com>
2023-07-24 17:13:49 -0400

    Update server hostname to support ipv6 for Nodejs >v16 https://github.com/vuestorefront/storefront-next13-boilerplate/issues/77

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,10 +1,10 @@
-import { createServer } from '@vue-storefront/middleware';
+import {createServer} from '@vue-storefront/middleware';
 import consola from 'consola';
 import config from '../middleware.config';
 
 (async () => {
   const app = await createServer({ integrations: config.integrations });
-  const host = process.argv[2] ?? '0.0.0.0';
+  const host = process.argv[2] ?? '::';
   const port = Number(process.argv[3]) || 4000;
 
   app.listen(port, host, () => {
